d3077a02d22c2bd63b3fea28e6fe6eb6f844f351
Andres <aldavcas@outlook.com>
2026-03-27 04:35:51 -0500

    creacion modelos adopciones y mascotas

--- adopciones/models.py
+++ adopciones/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from usuarios.models import Usuario
+from mascotas.models import Mascota, EstadoAdopcion
 
-# Create your models here.
+class Adopcion(models.Model):
+    id_adopcion = models.BigAutoField(primary_key=True)
+
+    adoptante = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        db_column='id_usuario'
+    )
+
+    mascota = models.ForeignKey(
+        Mascota,
+        on_delete=models.CASCADE,
+        db_column='id_mascota'
+    )
+
+    fecha_solicitud = models.DateField()
+    fecha_aprobacion = models.DateField(null=True, blank=True)
+
+    estado_adopcion = models.CharField(
+        max_length=20,
+        choices=EstadoAdopcion.choices,
+        default=EstadoAdopcion.PENDIENTE
+    )
+
+    url_formulario_descarga = models.CharField(max_length=255, null=True, blank=True)
+    formulario_enviado = models.BooleanField(default=False)
+    fecha_envio_formulario = models.DateTimeField(null=True, blank=True)
+    observaciones = models.TextField(null=True, blank=True)
+    fcha_registro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'adopciones'
+
+        def __str__(self):
+            return f"Adopcion: {self.mascota.nombre} -> {self.adoptante.nombre}"
