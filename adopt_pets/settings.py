2c35fcf26115ecba80e63aa87c70cfbe3d50146b
Andres <aldavcas@outlook.com>
2026-06-27 03:06:02 -0500

    fix de imagenes cloud 1.3

--- adopt_pets/settings.py
+++ adopt_pets/settings.py
@@ -105,7 +105,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ##
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
