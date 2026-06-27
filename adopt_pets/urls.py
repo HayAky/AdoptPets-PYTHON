09aa212f017ee04ef9e1cad1a143ab6e697fe7b7
Andres <aldavcas@outlook.com>
2026-06-27 02:40:27 -0500

    fix de imagenes cloud 1.1

--- adopt_pets/urls.py
+++ adopt_pets/urls.py
@@ -1,7 +1,7 @@
 """
-URL configuration for adopt_pets project.
+URL configuration for an adopt_pets project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
 Function views
