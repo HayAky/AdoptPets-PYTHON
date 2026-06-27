4c203c1d509c88da5315d6f8d949df3e7c9390c1
Andres <aldavcas@outlook.com>
2026-06-27 04:31:36 -0500

    fix fotos3

--- mascotas/forms.py
+++ mascotas/forms.py
@@ -3,7 +3,15 @@ from .models import Mascota
 
 class MascotaForm(forms.ModelForm):
     # Esto le dice a Django: "lo que venga como 'edadAproximada' va al campo 'edad_aproximada'"
-    edadAproximada = forms.IntegerField(source='edad_aproximada')
+    # En forms.py
+    edadAproximada = forms.IntegerField()
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.edad_aproximada = self.cleaned_data['edadAproximada']
+        if commit:
+            instance.save()
+        return instance
 
     class Meta:
         model = Mascota
