2d415f0169d8a42deb2d8491391fa2a7cfc07244
Andres <aldavcas@outlook.com>
2026-06-27 04:46:17 -0500

    fix fotos4

--- mascotas/forms.py
+++ mascotas/forms.py
@@ -2,17 +2,20 @@ from django import forms
 from .models import Mascota
 
 class MascotaForm(forms.ModelForm):
-    # Esto le dice a Django: "lo que venga como 'edadAproximada' va al campo 'edad_aproximada'"
-    # En forms.py
-    edadAproximada = forms.IntegerField()
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        instance.edad_aproximada = self.cleaned_data['edadAproximada']
-        if commit:
-            instance.save()
-        return instance
-
     class Meta:
         model = Mascota
         fields = '__all__'
+        widgets = {
+            # Aquí inyectas las clases CSS que tenías antes para recuperar el estilo
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'especie': forms.Select(attrs={'class': 'form-control'}),
+            'raza': forms.TextInput(attrs={'class': 'form-control'}),
+            'edad_aproximada': forms.NumberInput(attrs={'class': 'form-control'}),
+            'sexo': forms.Select(attrs={'class': 'form-control'}),
+            'tamano': forms.Select(attrs={'class': 'form-control'}),
+            'peso': forms.NumberInput(attrs={'class': 'form-control'}),
+            'color': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'estado_salud': forms.Textarea(attrs={'class': 'form-control'}),
+            'foto': forms.FileInput(attrs={'class': 'form-control'}),
+        }
